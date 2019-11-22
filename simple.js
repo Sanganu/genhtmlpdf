f33b2a491f3bddfd1968b7c23f44e3bcfc6810a6
Sanganu <kppkmsanu@gmail.com>
2019-11-22 15:47:57 -0700

    asyn issue

--- simple.js
+++ simple.js
@@ -17,7 +17,7 @@ const questions = [
 ]
    inquirer.prompt(questions)
     .then(response => {
-        console.log("Searching for profile ",response.github);
+       console.log("Searching for profile ",response.github);
         color = response.color;
         return response
     }).then(response => {
@@ -39,19 +39,19 @@ const questions = [
             }
            return generateHTML(details)
     }).then((html) => {
-            console.log("HTML generated",html);
+           // console.log("HTML generated",html);
             fs.writeFile("index.html",html,function(err,result){
                 if(err) console.log("Error in writing to index")
                 console.log("Result after writing Index html");
-                return html
+                const htmlfile = fs.readFileSync("./index.html","utf8");
+                const options ={ format: 'Letter'};
+                pdf.create(htmlfile,options).toFile("./profile.pdf",function(err,res){
+                    if (err) return console.log(err);
+                    console.log("File created",res);
+                });
+              //  return html
             });
-    }).then((result) => {
-            const htmlfile = fs.readFileSync("./index.html","utf8");
-            const options ={ format: 'Letter'};
-            pdf.create(htmlfile,options).toFile("./profile.pdf",function(err,res){
-                if (err) return console.log(err);
-                console.log("File created",res);
-            });
+          
     }).catch(error =>{
         console.log("Error ",error)
     });
